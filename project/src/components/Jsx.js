f1884b255a80c57dd6edfc852a1390797451d7a9
Naim Latifi <naimlatifi98@hotmail.com>
2019-01-14 21:34:02 +0100

    more enhancement for jsx example code

--- project/src/components/Jsx.js
+++ project/src/components/Jsx.js
@@ -5,8 +5,11 @@ const users = {
   person2: "Naim Latifi",
   employee: "HIQ"
 }
-
-//const emp = <h1>Emply is {users.employee}</h1>
+const headingId = 'myHeading'
+const paragraphStyle = {
+  color: "red"
+}
+const emp = <h1>Employee at {users.employee}</h1>
 
 function printUserNames(user) {
   return `${user.person1} ${user.person2}`
@@ -16,24 +19,26 @@ class Jsx extends React.Component {
     // jsx a javascript extension that allow as to write js code that looks like html 
     console.log("React object properties ", React);
     return (
-      // jsx syntax
-      <div>
-        <h1 className="heading">Hello Jsx</h1>
-        <p>Users name are: {printUserNames(users)}</p>
-      </div>
-      //emp
+      // jsx syntax and where javascript transpiles the code under the hood
+      // attributes in jsx are named as camelCase i.e className,  onClick, onSubmit etc...
+      <div class="parent-tag">
+        <h1 className="heading" id={headingId}>Hello Jsx</h1> 
+        <p style={paragraphStyle}>Users name are: {printUserNames(users)}</p>
+        <h2>{emp}</h2>
+          
+          {
+           //Jsx will transplate to this javascript code on runtime, if we do not want to use jsx as above we can then write pure js syntax as below
+            React.createElement('div', 
+            {className: 'container'},
+             React.createElement(
+              'h1',
+              {className: 'heading'},
+              'Hello World with Javascript'
+              )
+            )
+          }
 
-      //Jsx will transplate to this javascript code on runtime, if we do not want to use jsx as above we can then write pure js syntax as below
-      // React.createElement(
-      //   'div',
-      //   {className: 'container'},
-      //    null,
-      //    React.createElement(
-      //   'h1',
-      //   {className: 'heading'},
-      //   'Hello World with Javascript'
-      //   )
-      // )
+      </div>
     ) 
   }
 }
