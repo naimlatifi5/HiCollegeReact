623381e27860bf86f0e5a79bafdbbc69958c701a
Naim <naimla@HIQSTOM307.local>
2021-04-20 21:44:16 +0200

    refactoed

--- project/src/components/State/ChildComponent.js
+++ project/src/components/State/ChildComponent.js
@@ -1,6 +1,4 @@
-import React, {Component} from 'react'
-
- 
+import React, { Component } from 'react'
 
 class ChildState extends Component {
     constructor (props) {
@@ -12,17 +10,19 @@ class ChildState extends Component {
     }
     handleParentStateChanges = () => {
       this.setState(
-        {mutateParentState: this.props.name + " Latifi"}
+        { mutateParentState: this.props.name + " Latifi"}
       ) // mutate via setState the state that we get as props from parent
     }
   render() {
-    return <div>
-    <h1>Child component handling state</h1>
-    <p>Child state : {this.state.childState}</p>
-    <p>What is parent state : {this.props.name}</p>
-    <button onClick={this.handleParentStateChanges}>Child mutated the state from parent</button>
-    <p>state changes : {this.state.mutateParentState}</p>
-  </div>
+    return (
+      <div>
+        <h1>Child component handling state</h1>
+        <p>Child state : {this.state.childState}</p>
+        <p>What is parent state : {this.props.name}</p>
+        <button onClick={this.handleParentStateChanges}>Child mutated the state from parent</button>
+        <p>state changes : {this.state.mutateParentState}</p>
+      </div>
+    )
   }
 }
 
