484c5ac3ec0bea10dc2820f044cb0ec5cb0ca490
Naim <naimla@HIQSTOM307.local>
2021-04-20 20:54:44 +0200

    refactoed

--- project/src/components/State/BuyProduct.js
+++ project/src/components/State/BuyProduct.js
@@ -1,9 +1,8 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 
 // state only can be used within class Component and cannot be used with function component
 // state in React are immutable- do not change directly but change should be made to the copy of the state.
 // state are kind of like props but state is private property of the component and is controlled only by that component.
-
 
 class BuyProduct extends Component {
     constructor (props) {
@@ -14,9 +13,17 @@ class BuyProduct extends Component {
    }
    buy = () => {
     this.setState({
-      quantity: this.state.quantity + 1
+        quantity: this.state.quantity + 1
     })
    } 
+  render() {
+    return (
+      <div>
+        <strong>Quantity {this.state.quantity} item(s)</strong><br/>
+        <button className="btn btn-primary" onClick={this.buy}>Buy</button>
+      </div>
+    )
+  }
 }
 
 export default BuyProduct
