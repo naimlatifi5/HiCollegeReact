653c1467694390eea1818373d56ab02dc0a1cda8
Naim <naimla@HIQSTOM307.local>
2021-05-01 10:43:01 +0200

    added useEffect with hooks

--- project/src/components/hooks/ClassComponent.js
+++ project/src/components/hooks/ClassComponent.js
@@ -5,19 +5,40 @@ class ClassComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          toggle: false
+          toggle: false,
+          name: "Superman"
         }
     }
     setToggle = () => {
        this.setState({ toggle: !this.state.toggle })
     }
+    handleChange = (e) => {
+      this.setState({
+         name: e.target.value
+      })
+    }
+    componentDidMount() {
+      document.title = this.state.name
+    }
+    componentDidUpdate() {
+       document.title = this.state.name
+    }
     render() {
         return (
           <div className="class-component">
-            <button type="button" onClick={ this.setToggle }>
-                Toggle me
+            <button type="button" onClick={this.setToggle}>
+              Toggle me
             </button>
-            { this.state.toggle ? <div> This is a div to toggle </div> : null }
+            {this.state.toggle ? (
+              <div> This is a div to toggle </div>
+            ) : null}
+            <br />
+            <br />
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           </div>
         ) 
     }
--- project/src/components/hooks/HooksCompnent.js
+++ project/src/components/hooks/HooksCompnent.js
@@ -1,18 +1,31 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
- const HooksComponent = () => {
+const HooksComponent = () => {
+
   // Declare a new state variable, which we'll call "toggle"
     const [toggle, setToggle] = useState(false)
   // multipe hooks declarations
     const [fruit, setFruit] = useState('banana');
     //const [todos, setTodos] = useState([{ text: "Learn Hooks" }])
-   
+   const [name, setName] = useState("Superman")
     const toggleTrueFalse = () => setToggle(!toggle)
    
    const getFruit = () => {
       // no need for this but accessing variable directly
       setFruit('apple')
     }
+   const handleChangeName = (e) => {
+     setName(e.target.value)
+   }
+
+   // Similar to componentDidMount and componentDidUpdate:
+   useEffect(() => {
+     document.title = name
+   })
+   // re-trigger only once with empty []
+  //  useEffect(() => {
+  //     document.title = name
+  //  }, [])
    
     return (
       <div className="hooks">
@@ -20,10 +33,12 @@ import React, { useState } from "react"
           Toggle me
         </button>
         {toggle ? <div> This is a div to toggle via hooks </div> : null}
-        <div>Fruit is: { fruit }</div>
-        <button type="button" onClick={ getFruit }>
+        <div>Fruit is: {fruit}</div>
+        <button type="button" onClick={getFruit}>
           Click to change fruit
         </button>
+        <br />
+        <input type="text" value={name} onChange={ handleChangeName } />
       </div>
     )
 }
