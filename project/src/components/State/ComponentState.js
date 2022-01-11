ffe3f5b60bb4f44b35f165d19a628c829a16c648
Naim <naim.latifi@hiq.se>
2022-01-11 22:27:26 +0100

    refactored on state

--- project/src/components/State/ComponentState.js
+++ project/src/components/State/ComponentState.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import ChildState from './ChildComponent'
 // state only can be used within class Component and cannot be used with function component: (UPDATED) but not anymore we can use hooks with new version of react to have state
-// state in React are immutable- do not change directly but change should be made to the copy of the state.
+// state in React are immutable - do not change directly but change should be made with setState.
 // state are kind of like props but state is private property of the component and is controlled only by that component.
 
 class ComponentState extends Component {
-  // to add a state create a constructor function that initalize the state
+  // to add a state create a constructor function that initialize the state
   // it is recommended even thought we do not send props to component to assign in the constructor function
   constructor(props) {
     super(props) // super is a must when using constructor - for more read about how works with Inheritance in OOP
@@ -16,9 +15,8 @@ class ComponentState extends Component {
   render() {
     return (
       <div>
-        <h1>Component with state and child component</h1>
+        <h1>Component with state</h1>
         <p>What is my name : {this.state.name}</p>
-        <ChildState name={this.state.name}></ChildState>
       </div>
     )
   }
--- project/src/components/State/Main.js
+++ project/src/components/State/Main.js
@@ -1,16 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ComponentState from './ComponentState'
 import BuyProduct from './BuyProduct'
 class MainState extends Component {
-
   render() {
     return (
-    <div className="product-wrapper">
-      <ComponentState name="hello prop"></ComponentState>
-      <hr/>
-      <BuyProduct></BuyProduct>
-    </div>
-  )
+      <div className="product-wrapper">
+        <ComponentState></ComponentState>
+        <hr />
+        <BuyProduct></BuyProduct>
+      </div>
+    )
   }
 }
 
