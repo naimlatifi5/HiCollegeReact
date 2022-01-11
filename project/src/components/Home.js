82a2b58178ef14646e4f86851e3cebc1f1bf2e10
Naim <naim.latifi@hiq.se>
2022-01-11 22:37:58 +0100

    fixed refactoring with props

--- project/src/components/Home.js
+++ project/src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg';
+import logo from '../logo.svg'
 
 class Home extends Component {
   render() {
-    return <div>
-    <img src={logo} className="App-logo" alt="logo" />
-    <h1>Welcome to HIQ React course</h1>
-  </div>
+    return (
+      <>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to HIQ React course</h1>
+      </>
+    )
   }
 }
 
--- project/src/components/Props/FunctionComponentProps.js
+++ project/src/components/Props/FunctionComponentProps.js
@@ -2,19 +2,23 @@ import React from 'react'
 
 // components with props and ES6 function declaration
 // props in functional components are received via props function arguments
-const FunctionComponentProps = (props) => {
+// or with the help of destructing you can access the prop name in function argument
+// default props are assigned
+const FunctionComponentProps = ({
+  imageUrl = '#',
+  buttonText = 'no button text set',
+  href = '#',
+  text = 'no text is set',
+}) => {
   return (
     <div className="block-container">
-      <img
-        src={props.imageUrl}
-        className="block-container__image"
-        alt="react"
-      />
+      <img src={imageUrl} className="block-container__image" alt="react" />
       <div className="block-container__content">
-        <p>{props.text || 'no text is set'}</p> {/* passing default props */}
+        <p>{text}</p>{' '}
+        {/* passing default props or default props can also be used as text || "this is a default prop" */}
       </div>
-      <a href={props.href} className="btn">
-        {props.buttonText}
+      <a href={href} className="btn">
+        {buttonText}
       </a>
     </div>
   )
