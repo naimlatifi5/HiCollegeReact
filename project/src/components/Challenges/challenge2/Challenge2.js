49ef1229072578bdc49f3387c15d88721f772251
Naim <naim.latifi@hiq.se>
2022-01-15 21:48:41 +0100

    added challenge 2

--- project/src/components/Challenges/challenge2/Challenge2.js
+++ project/src/components/Challenges/challenge2/Challenge2.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Challenge2 = () => {
-  return <div>Challenge2</div>
+const onlineCourses = [
+  { id: 1, name: 'JavaScript' },
+  { id: 2, name: 'HTML' },
+  { id: 3, name: 'Vue.js' },
+  { id: 4, name: 'React.js' },
+  { id: 5, name: 'CSS' },
+]
+class Challenge2 extends Component {
+  // write your magic below this line :)
+  render() {
+    return <div></div>
+  }
 }
-
 export default Challenge2
