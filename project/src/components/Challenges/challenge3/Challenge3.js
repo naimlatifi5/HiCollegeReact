abb5a14a87edd3d62991be2c8218c53678f37fde
Naim <naim.latifi@hiq.se>
2022-01-26 21:41:28 +0100

    added challenge 3

--- project/src/components/Challenges/challenge3/Challenge3.js
+++ project/src/components/Challenges/challenge3/Challenge3.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const Challenge3 = () => {
-  return <div>Challenge3</div>
+class Challenge3 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Build a simple quantity counter</h1>
+      </div>
+    )
+  }
 }
 
 export default Challenge3
