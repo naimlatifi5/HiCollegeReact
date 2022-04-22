7536d5daa201dce589b9c2251a846a0ee2626cf3
Rubihiq <rubi.kuo@hiq.se>
2022-04-22 11:50:59 +0200

    changed challenge component in main.js

--- project/src/components/Challenges/Main.js
+++ project/src/components/Challenges/Main.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import Challenge1 from './challenge1/Challenge1'
-import Challenge2 from './challenge2/Challenge2'
-import Challenge3 from './challenge3/Result/Challenge3-3'
-import Challenge4 from './challenge4/Challenge4'
-import Challenge5 from './challenge5/Challenge5'
+import React, { Component } from "react";
+import Challenge1 from "./challenge1/Challenge1";
+import Challenge2 from "./challenge2/Challenge2";
+import Challenge3 from "./challenge3/Challenge3";
+import Challenge4 from "./challenge4/Challenge4";
+import Challenge5 from "./challenge5/Challenge5";
 class MainChallenge extends Component {
   render() {
     return (
@@ -18,8 +18,8 @@ class MainChallenge extends Component {
         <hr />
         <Challenge5 />
       </div>
-    )
+    );
   }
 }
 
-export default MainChallenge
+export default MainChallenge;
